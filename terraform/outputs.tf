ebcbbb68e2c53dd36eb97aad20a9f224451a8191
Your Name <you@example.com>
2025-05-25 14:35:23 +0300

    ✨ Fixed dynamic IP SSH and Terraform flow

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,5 @@
 output "instance_public_ip" {
-  description = "The public IP of the EC2 instance"
+  description = "The public IP address of the EC2 instance"
   value       = aws_instance.devops_instance.public_ip
+  sensitive   = false
 }
