32c620f43a230eabfd0374208061a33bfed6eeb6
Your Name <you@example.com>
2025-05-25 15:17:29 +0300

    🚀 Replace remote-exec with user_data for EC2 bootstrap

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,31 +5,24 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-
 resource "tls_private_key" "example" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-
-
 
 resource "aws_instance" "devops_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
 
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = tls_private_key.example.private_key_pem
-      host        = self.public_ip
-    }
-
-    inline = [
-      "docker run -d -p 3000:3000 taltal1131/devops-full-app:latest"
-    ]
-  }
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install docker.io -y
+              systemctl start docker
+              systemctl enable docker
+              docker run -d -p 3000:3000 taltal1131/devops-full-app:latest
+            EOF
 
   tags = {
     Name = "DevOpsInstance"
