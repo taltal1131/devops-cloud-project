129a45ecde79a3d97b5c64e31718f24c5728ce3f
Your Name <you@example.com>
2025-05-25 15:03:25 +0300

    ✅ Fix: Add all required variables to variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,36 @@
 
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret access key"
+  type        = string
+}
+
 variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
+  description = "AMI ID to use for the EC2 instance"
   type        = string
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "Type of EC2 instance"
   type        = string
   default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of the key pair in AWS"
+  type        = string
+}
+
+variable "ec2_private_key" {
+  description = "Private key used to SSH into the EC2 instance"
+  type        = string
 }
